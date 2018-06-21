29a3d6371f57e8eb24a55577f32d2f257d000bd4
tomvanwoow <tom@vanhowe.com>
2018-06-21 11:36:19 +0100

    1.0 Release

--- src/main.go
+++ src/main.go
@@ -103,6 +103,13 @@ func messageCreate(session *discordgo.Session, message *discordgo.MessageCreate)
 	}()
 	fmt.Println(message.Content)
 	if !message.Author.Bot {
+		if strings.ToLower(message.Content) == "good bot" {
+			m, _ := session.ChannelMessageSend(message.ChannelID, "Your compliments mean nothing to me")
+			time.Sleep(5 * time.Second)
+			if m != nil {
+				session.ChannelMessageDelete(message.ChannelID, m.ID)
+			}
+		}
 		if strings.HasPrefix(strings.ToLower(message.Content), prefix) {
 			err := commands.ExecuteCommand(session, message, bot)
 			if err != nil {
@@ -111,16 +118,6 @@ func messageCreate(session *discordgo.Session, message *discordgo.MessageCreate)
 		}
 		grantExp(&bot, session, message)
 		fmt.Println("-----------------------")
-	}
-}
-
-func guildCreate(_ *discordgo.Session, event *discordgo.GuildCreate) {
-	for {
-		if bot.Guilds != nil {
-			commands.CreateAllData(db, event.Guild.ID)
-			bot.Guilds.Get(event.Guild.ID)
-			break
-		}
 	}
 }
 
@@ -255,7 +252,6 @@ func main() {
 	}
 	dg.AddHandler(ready)
 	dg.AddHandler(messageCreate)
-	dg.AddHandler(guildCreate)
 	dg.AddHandler(memberAdd)
 	err = dg.Open()
 	if err != nil {
@@ -267,5 +263,8 @@ func main() {
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
-	commands.PostAllData(db, guilds)
+	err = commands.PostAllData(db, guilds)
+	if err != nil {
+		err = commands.PostAllData(db, guilds)
+	}
 }
