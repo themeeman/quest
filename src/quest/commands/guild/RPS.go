8009fbe46e076916854743b6ce19683b4e528906
tomvanwoow <tom@vanhowe.com>
2018-07-13 18:00:14 +0100

    minor changes

---
+++ src/quest/commands/guild/RPS.go
@@ -0,0 +1,10 @@
+package guild
+
+import (
+	"github.com/bwmarrin/discordgo"
+	commands "../../../discordcommands"
+)
+
+func Cmd(session *discordgo.Session, message *discordgo.MessageCreate, args map[string]string, bot commands.Bot) commands.BotError {
+	return nil
+}
